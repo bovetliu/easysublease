62d5cb82e4c0f506b282facd058687ebbff355fe
bovetliu <bowei_liu@ymail.com>
2015-06-05 00:58:59 -0500

    delted bf

--- bf/ZK14a7vX.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-  $hn = 'localhost';
-  $db = 'easysubl_easysublease';
-  $un = 'easysubl_jim';
-  $pw = 'jim123^&*';
-?>
--- bf/login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-  $hn = 'localhost';
-  $db = 'publications';
-  $un = 'jim';
-  $pw = 'jim123^&*';
-?>
